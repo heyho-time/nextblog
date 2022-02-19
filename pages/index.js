732966b58c06cf154ef351f380cc4255315436a9
Heyho Seong <heyho9292@gmail.com>
2022-02-19 23:38:19 +0900

    ADD : quote

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,7 @@ export default function Home({ allPostsData }) {
         <title>Heyho world</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Welcome to heyho's world ~</p>
-        <p>...</p>
+        <p>Welcome to heyho&#39;s world</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
